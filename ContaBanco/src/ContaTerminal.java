ac86c526b4891d52d1903a6cc297c8ee52a3110a
viniaquino <viniaquino@users.noreply.github.com>
2024-08-21 00:03:33 -0300

    Projeto finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,37 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    private int numeroConta;
+    private String agencia;
+    private String nomeCliente;
+    private double saldo;
+
+    public void abrirConta() {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da conta: ");
+        this.numeroConta = scanner.nextInt();
+        scanner.nextLine();
+        
+        System.out.println("Por favor, digite o numero da agência: ");
+        this.agencia = scanner.nextLine();
+        
+        System.out.println("Por favor, digite o nome do titular da conta: ");
+        this.nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo inicial: ");
+        this.saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + this.nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+        + this.agencia + ", conta " + this.numeroConta + " e seu saldo " + this.saldo + " já está disponível para saque.");
     }
+
+    public static void main(String[] args) {
+        ContaTerminal conta = new ContaTerminal();
+        conta.abrirConta();
+    }
+
 }
+
